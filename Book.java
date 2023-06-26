34b8bb4351ed16319185638cb560b9ca701c5c63
Dev1 - filip <pelloiafilippo@live.com>
2023-06-26 21:40:46 +0200

    second commit

--- Book.java
+++ Book.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+
 class Book {
     public static void main(String[] args){
+        User youngerUser = new User();
 
+        youngerUser.name = "Gennaro Rossi";
+        youngerUser.birthDay = LocalDate.parse("1995-01-31");
+
+        System.out.printf("%s was born in %s", youngerUser.name, youngerUser.birthDay);
     }
 }
---
+++ User.java
@@ -0,0 +1,6 @@
+import java.time.LocalDate;
+
+public class User {
+    public String name;
+    public LocalDate birthDay;
+}
