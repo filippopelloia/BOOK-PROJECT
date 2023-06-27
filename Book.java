8a24226f8b41b8aebc64800bf60bcbbc9d27b11f
Dev1 - filip <pelloiafilippo@live.com>
2023-06-27 12:44:37 +0200

    second commit tuesday

--- Book.java
+++ Book.java
@@ -1,7 +1,23 @@
 public class Book {
 
-    public String title;
-    public String author;
+    private String title;
+    private String author;
+
+    //COSTRUTTORE
+    Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public String getAuthor(){
+        return this.author;
+    }
+    
 
     //QUESTO IMPOSTA L'ASPETTO DELL'OGGETTO BOOK
     public String toString(){
